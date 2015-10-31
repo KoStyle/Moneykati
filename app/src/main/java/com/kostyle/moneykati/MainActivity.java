49baacadc8c6eefa08229c38b76b35844c81c06f
KoStyle <konomi360@gmail.com>
2015-10-31 15:55:34 +0100

    AppBar

--- app/src/main/java/com/kostyle/moneykati/MainActivity.java
+++ app/src/main/java/com/kostyle/moneykati/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     @Override
